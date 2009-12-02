797f37b676c8e99d4540f8c559a34cb7361c8495
snl <snl@theorydot.com>
2009-12-02 15:04:08 +0000

    Refactored participations controller

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,22 +1,24 @@
-class ParticipationsController < InheritedResources::Base
+class ParticipationsController < ApplicationController
   before_filter :require_user
-  
-  belongs_to :project
+  before_filter :find_project
   
   helper_method :can_remove?
   
+  def index
+    @participations = @project.participations
+  end
+  
   def new
-    new! {
-      @nonparticipants = @current_user.workmates.reject{ |w| @project.participants.include?(w) }
-      if @nonparticipants.empty?
-        flash[:error] = 'All your workmates are already participating in this project.'
-        return redirect_to(:action => :index)
-      end
-    }
+    @participants = @current_user.workmates.reject{ |w| @project.participants.include?(w) }
+    if @participants.empty?
+      flash[:error] = 'All your workmates are already participating in this project.'
+      return redirect_to(:action => :index)
+    end
+    @participation = Participation.new
   end
   
   def create
-    @project ||= @current_user.projects.find(params[:project_id])
+    # Existing workmate
     if params[:participation][:participant_id]
       participant = @current_user.workmates.find(params[:participation][:participant_id])
       if participant
@@ -25,32 +27,35 @@ class ParticipationsController < InheritedResources::Base
       else
         flash[:error] = 'Person not added to project!'
       end
+    
+    # New invitation
     elsif params[:participation][:invitee_email]
       flash[:error] = 'This feature is not active'
     else
       flash[:error] = 'Person not added to project!'
     end
-    return(redirect_to collection_url(@project))
+    
+    return(redirect_to project_participations_path(@project))
   end
   
   def destroy
-    @project ||= @current_user.projects.find(params[:project_id])
-    participant = resource.participant
-    if can_remove?(participant) && @project.participations.find_by_participant_id(participant.id).destroy
+    participation = @project.participations.find(params[:id])
+    participant = participation.participant
+    if can_remove?(participant) && participation.destroy
       flash[:notice] = 'Person removed from project!'
       if i_am?(participant)
-        return(redirect_to projects_url)
+        return(redirect_to projects_path)
       end
     else
       flash[:error] = 'Person not removed from project!'
     end
-    redirect_to collection_url
+    redirect_to project_participations_path
   end
   
   private
   
-  def begin_of_association_chain
-    @current_user
+  def find_project
+    @project = @current_user.projects.find(params[:project_id])
   end
   
   def can_remove?(participant)
