cbbd6a35dede21aacef0f51172e5e162800594a2
snl <snl@theorydot.com>
2009-12-04 20:31:35 +0000

    Adding Google Charts (wip)

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -13,4 +13,14 @@ module ProjectsHelper
     end
     links.join(' ')
   end
+  
+  def chart(project)
+    lc = GoogleChart::LineChart.new("400x200", "My Results", false)
+    lc.data "Line green", [3,5,1,9,0,2], '00ff00'
+    lc.data "Line red", [2,4,0,6,9,3], 'ff0000'
+    lc.axis :y, :range => [0,10], :font_size => 10, :alignment => :center
+    lc.show_legend = false
+    lc.shape_marker :circle, :color => '0000ff', :data_set_index => 0, :data_point_index => -1, :pixel_size => 10
+    lc.to_url
+  end
 end
