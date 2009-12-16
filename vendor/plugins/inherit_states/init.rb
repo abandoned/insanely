bcdcc14b95fb6330da103717dfb78f4b5ad25c30
snl <snl@theorydot.com>
2009-12-16 14:09:15 +0000

    Refactored inherit_states

--- vendor/plugins/inherit_states/init.rb
+++ vendor/plugins/inherit_states/init.rb
@@ -1,9 +1,9 @@
 class ActionController::Base
   def self.inherit_states
     resource_class.aasm_states.each do |state|
-      has_scope(state.name, :only => [state.name])
+      has_scope(state.name, :boolean => true, :only => [state.name])
       define_method(state.name) do
-        params[state.name] = 1
+        params[state.name] = true
         collection
         render :action => :index
       end
